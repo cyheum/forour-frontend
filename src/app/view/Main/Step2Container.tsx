9f1098963fbe19092d090d0ae532d81957d5b274
Yeheum Choi <yh.choi@purple-nest.com>
2021-10-10 04:44:45 +0900

    [ENH] : step2 수정 완료

--- src/app/view/Main/Step2Container.tsx
+++ src/app/view/Main/Step2Container.tsx
@@ -1,5 +1,5 @@
-import React,{useEffect} from 'react';
-import styled from 'styled-components';
+import React, { useEffect, useState } from 'react';
+import styled, { css } from 'styled-components';
 import { useRecoilState } from 'recoil';
 import { receiverState, receiverTypeState, errorTextState } from '@/store/main';
 import { mixins } from '@/styles';
@@ -20,9 +20,19 @@ const STDDescription = styled.p`
   color: #4d4d4d;
 `;
 
-const STDSelectContainer = styled.article`
-  ${mixins.flexSet('center', 'center', 'column')}
-  margin: 6.4375rem 0 10.28125rem;
+const STDSelectContainer = styled.article<{ innerHeight: number | null }>`
+  ${({ innerHeight }) =>
+    innerHeight &&
+    css`
+      height: ${innerHeight - 139.75 * 2}px;
+    `}
+  ${mixins.flexSet('flex-start', 'center')}
+  min-height: 22.5rem;
+
+  > div {
+    ${mixins.flexSet('center', 'center', 'column')}
+    width: 100%;
+  }
 `;
 
 const STDMyImage = styled.img`
@@ -85,11 +95,15 @@ const STDSelectResult = styled.div`
 
 const STDNextButton = styled.div`
   ${mixins.flexSet()}
+  position: fixed;
+  bottom: 3rem;
+  left: 50%;
+  transform: translate(-50%, 0);
 
   button {
     font-family: Pretendard;
-    font-size: 1rem;
     color: black;
+    font-size: 1rem;
     line-height: 1.625rem;
     text-decoration: underline;
   }
@@ -99,15 +113,18 @@ const Step2Container: React.FC<IProps> = ({ goToNext }) => {
   const [receiver, setReceiver] = useRecoilState(receiverState);
   const [receiverType, setReceiverType] = useRecoilState(receiverTypeState);
   const [errorText, setErrorText] = useRecoilState(errorTextState);
+  const [windowHeight, setWindowHeight] = useState<number | null>(null);
+
+  useEffect(() => {
+    setWindowHeight(window.innerHeight);
+  }, []);
 
   const setReceiverInfoInSessionStorage = () => {
-        const receiverInfo = JSON.stringify({
-      receiver
-    })
-    sessionStorage.setItem("receiver", receiverInfo)
-  }
-
- 
+    const receiverInfo = JSON.stringify({
+      receiver,
+    });
+    sessionStorage.setItem('receiver', receiverInfo);
+  };
 
   return (
     <>
@@ -115,36 +132,40 @@ const Step2Container: React.FC<IProps> = ({ goToNext }) => {
       <STDDescription>
         친구나 애인을 선택하고 이름을 입력해주세요
       </STDDescription>
-      <STDSelectContainer>
-        <STDMyImage alt="my image" src="/my.png" />
-        <STDSelectWrapper>
-          <STDSelectBox
-            isSelected={receiverType === 'friend'}
-            onClick={() => setReceiverType('friend')}
-          >
-            <img
-              src={`/friend_${
-                receiverType === 'friend' ? 'white' : 'gray'
-              }.png`}
+      <STDSelectContainer innerHeight={windowHeight}>
+        <div>
+          <STDMyImage alt="my image" src="/my.png" />
+          <STDSelectWrapper>
+            <STDSelectBox
+              isSelected={receiverType === 'friend'}
+              onClick={() => setReceiverType('friend')}
+            >
+              <img
+                src={`/friend_${
+                  receiverType === 'friend' ? 'white' : 'gray'
+                }.png`}
+              />
+            </STDSelectBox>
+            <STDSelectBox
+              isSelected={receiverType === 'lover'}
+              imgWidth={4.0625}
+              onClick={() => setReceiverType('lover')}
+            >
+              <img
+                src={`/lover_${
+                  receiverType === 'lover' ? 'white' : 'gray'
+                }.png`}
+              />
+            </STDSelectBox>
+          </STDSelectWrapper>
+          <STDSelectResult>
+            <input
+              value={receiver}
+              onChange={(e) => setReceiver(e.target.value)}
             />
-          </STDSelectBox>
-          <STDSelectBox
-            isSelected={receiverType === 'lover'}
-            imgWidth={4.0625}
-            onClick={() => setReceiverType('lover')}
-          >
-            <img
-              src={`/lover_${receiverType === 'lover' ? 'white' : 'gray'}.png`}
-            />
-          </STDSelectBox>
-        </STDSelectWrapper>
-        <STDSelectResult>
-          <input
-            value={receiver}
-            onChange={(e) => setReceiver(e.target.value)}
-          />
-          {receiver.length === 0 && <img src="/name_gray.png" />}
-        </STDSelectResult>
+            {receiver.length === 0 && <img src="/name_gray.png" />}
+          </STDSelectResult>
+        </div>
       </STDSelectContainer>
       <STDNextButton>
         <button
@@ -154,8 +175,7 @@ const Step2Container: React.FC<IProps> = ({ goToNext }) => {
             } else {
               errorText.length > 0 && setErrorText('');
               goToNext();
-              setReceiverInfoInSessionStorage()
-
+              setReceiverInfoInSessionStorage();
             }
           }}
         >
